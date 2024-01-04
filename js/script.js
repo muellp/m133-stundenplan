f8f851875dc0854812b87b442e906859634e8123
philip <dadelphilip@hotmail.de>
2024-01-04 19:17:33 +0100

    get professions

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ const apiProfession = "http://sandbox.gibm.ch/berufe.php"
 const apiClass = "http://sandbox.gibm.ch/klassen.php"
 const apiTable = "http://sandbox.gibm.ch/tafel.php"
 
+function getProfessions() {
+    $.ajax({
+        url: apiProfession,
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            data.forEach(element => {
+                console.log(element)
+            });
+        },
+        error: function (xhr, status, error) {
+            alert('Error:', status, error);
+        }
+    });
+
+}
+
+getProfessions();
